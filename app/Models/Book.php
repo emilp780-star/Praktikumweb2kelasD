da1861d9288546401bb5cb24c7b6364ef0b0bb07
emilp780-star <emilp780@gmail.com>
2026-06-24 14:23:32 +0800

    menentukan relasi dan casts pada model book

---
+++ app/Models/Book.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    protected $fillable = [
+        'category_id',
+        'title',
+        'author',
+        'release_date',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'release_date' => 'date',
+        ];
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function borrows()
+    {
+        return $this->hasMany(Borrow::class);
+    }
+}
